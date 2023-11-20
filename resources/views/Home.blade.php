7bc50148ea6228c428795d3c34d5127c7fb3c64a
Loris Infante <lorisinfante@MacBook-Pro-di-Loris.local>
2023-11-20 15:42:31 +0100

    ora puoi pullare

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -6,12 +6,13 @@
             <div class="d-flex justify-content-center align-items-center col-12">
                 <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
                     <div class="carousel-inner">
-                        <div class="background-home"></div>
+                        <div class="{{-- background-home --}}"></div>
                         <div class="carousel-item active carousel-size">
                             <video loop="" autoplay="" muted="muted" src="/media/video/x2mate.com-Marvel's Spider-Man 2 - Trailer di lancio _ PS5 Games-(1080p).mp4"></video>
                         </div>
                         <div class="carousel-item carousel-size">
-                            <video loop="" autoplay="" muted="muted" src="/media/video/x2mate.com-Baldur's Gate 3 - Reveal Trailer _ PS5 Games-(1080p60).mp4"></video>
+                            <video loop="" autoplay="" muted="muted" src="/media/video/x2mate.com-Baldur's Gate 3 - Reveal Trailer _ PS5 Games-(1080p60).mp4">
+                            </video>
                         </div>
                         <div class="carousel-item carousel-size">
                             <video loop="" autoplay="" muted="muted" src="/media/video/x2mate.com-Super Mario Bros. Wonder - Official Launch Trailer-(1080p60).mp4"></video>
