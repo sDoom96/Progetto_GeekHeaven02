67a6d4d020659c118a577974afbcc437ab512d0a
Loris Infante <lorisinfante@MacBook-Pro-di-Loris.local>
2023-11-21 15:43:36 +0100

    prove swiper fallite

--- resources/views/Home.blade.php
+++ resources/views/Home.blade.php
@@ -1,28 +1,32 @@
 <x-layout>
     
     <header>
-    <div class="container-fluid">
-        <div class=" row-custom row">
-            <div class="d-flex justify-content-center align-items-center col-12">
-                <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
-                    <div class="carousel-inner">
-                        <div class="{{-- background-home --}}"></div>
-                        <div class="carousel-item active carousel-size">
-                            <video loop="" autoplay="" muted="muted" src="/media/video/x2mate.com-Marvel's Spider-Man 2 - Trailer di lancio _ PS5 Games-(1080p).mp4"></video>
-                        </div>
-                        <div class="carousel-item carousel-size">
-                            <video loop="" autoplay="" muted="muted" src="/media/video/x2mate.com-Baldur's Gate 3 - Reveal Trailer _ PS5 Games-(1080p60).mp4">
-                            </video>
-                        </div>
-                        <div class="carousel-item carousel-size">
-                            <video loop="" autoplay="" muted="muted" src="/media/video/x2mate.com-Super Mario Bros. Wonder - Official Launch Trailer-(1080p60).mp4"></video>
+        {{-- <div class="container-fluid">
+            <div class=" row-custom row">
+                <div class="d-flex justify-content-center align-items-center col-12">
+                    <div id="carouselExampleSlidesOnly" class="carousel slide" data-bs-ride="carousel">
+                        <div class="carousel-inner">
+                            <div class="background-home"></div>
+                            <div class="carousel-item active carousel-size">
+                                <video loop="" autoplay="" muted="muted" src="/media/video/x2mate.com-Marvel's Spider-Man 2 - Trailer di lancio _ PS5 Games-(1080p).mp4"></video>
+                            </div>
+                            <div class="carousel-item carousel-size">
+                                <video loop="" autoplay="" muted="muted" src="/media/video/x2mate.com-Baldur's Gate 3 - Reveal Trailer _ PS5 Games-(1080p60).mp4">
+                                </video>
+                            </div>
+                            <div class="carousel-item carousel-size">
+                                <video loop="" autoplay="" muted="muted" src="/media/video/x2mate.com-Super Mario Bros. Wonder - Official Launch Trailer-(1080p60).mp4"></video>
+                            </div>
                         </div>
                     </div>
                 </div>
-            </div>
-        </div>
-    </header>
+            </div> --}}
 
+        
+        
+        
+    </header>
+    
     
     
     
